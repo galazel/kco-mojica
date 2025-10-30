8ac6402155efc48d321150a9375e34939c41450e
Glyzel Galagar <133832079+galazel@users.noreply.github.com>
2025-10-31 07:22:28 +0800

    added login and register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
 import Landing from "./views/Landing";
 import Checkout from "./views/Checkout"
-import Account from "./views/Account"
 
 import { Routes, Route } from "react-router-dom";
+import Login from "./views/Login";
+import Signup from "./views/Signup";
+import Admin from "./views/Admin";
 
 function App() {
   return <>
     <Routes>
         <Route path="/" element={<Landing/>}></Route>
         <Route path="/checkout" element={<Checkout/>}></Route>
-        <Route path="/account" element={<Account/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/signup" element={<Signup/>}></Route>
+        <Route path="/admin" element={<Admin/>}></Route>
     </Routes>
   </>;
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,39 +5,55 @@ import {
   faCartShopping,
   faBagShopping,
 } from "@fortawesome/free-solid-svg-icons";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 function Header() {
-  const navigate = useNavigate();
-  const checkout = () => {
-    navigate("/checkout");
-  };
-  const home = () => {
-    navigate("/");
-  };
-  const account = () => {
-    navigate("/account");
-  };
   return (
     <header className="sticky top-0 bg-green-800 flex justify-center">
       <div className="w-[50vw] flex justify-between items-center">
-        <img
-          src="/kco_logo.png  "
-          className="w-[8.6rem] cursor-pointer"
-          alt=""
-          onClick={home}
-        />
+        <Link to="/">
+          <img
+            src="/kco_logo.png  "
+            className="w-[8.6rem] cursor-pointer"
+            alt=""
+          />
+        </Link>
         <div className="flex gap-5">
-          <FontAwesomeIcon
-            icon={faCartShopping}
-            className="text-4xl cursor-pointer text-white"
-            onClick={checkout}
-          />
-          <FontAwesomeIcon
-            icon={faUser}
-            className="text-4xl cursor-pointer text-white"
-            onClick={account}
-          />
+          <Link to="/checkout">
+            {" "}
+            <FontAwesomeIcon
+              icon={faCartShopping}
+              className="text-4xl cursor-pointer text-white"
+            />
+          </Link>
+
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <FontAwesomeIcon
+                icon={faUser}
+                className="text-4xl cursor-pointer text-white"
+              />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <Link to="/login">
+                <DropdownMenuItem>Login</DropdownMenuItem>
+              </Link>
+              <Link to="/signup">
+                {" "}
+                <DropdownMenuItem>Signup</DropdownMenuItem>
+              </Link>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </div>
     </header>
